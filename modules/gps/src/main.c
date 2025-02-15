bb13dacb5fe24a7906cfc93ede4caad717f01249
Andrew Sayanlar <andrewsayanlar@gmail.com>
2025-02-15 15:07:11 -0500

    feat: created a UART receive function and it is called in main.
    Created a UART receive function that gathers data in 200 ms intervals. The
    UART receive function is called in the main function via the xTaskCreate
    function.

--- modules/gps/src/main.c
+++ modules/gps/src/main.c
@@ -8,7 +8,6 @@ static const int RX_BUFFER_SIZE = 128;
 void uart_init(void) {
 
     const uart_config_t uart_config = {
-
         .baud_rate  = UART_DATA_8_BITS,
         .data_bits = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
@@ -20,8 +19,24 @@ void uart_init(void) {
     uart_driver_install(UART_NUM_0, RX_BUFFER_SIZE, 0, 0, NULL, 0);
 }
 
+static void rx_uart_data(){
+
+    const char *RX_UART_TAG = "RX UART"
+    esp_log_level_set(RX_UART_TAG, ESP_LOG_INFO);
+    uint8_t* data = malloc((RX_BUFFER_SIZE+1)*sizeof(uint8_t))
+    while (1){
+
+        int rx_bytes = uart_read_bytes(UART_NUM_0, data, RX_BUFFER_SIZE, 200 / portTICK_RATE_MS);
+        if (rx_bytes > 0){
+            data[rx_bytes] = 0;
+            ESP_LOGI(RX_UART_TAG, ("Read %d bytes: '%s'", rx_bytes, data));
+        }
+    }
+    free(data);
+}
+
 void main(void) {
 
     uart_init();
-
+    xTaskCreate(rx_uart_data, "rx_uart_data_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 }
