f079a94a6229e4e9feb1c905fa4af080682b80a7
Andrew Sayanlar <72060694+asayanlar@users.noreply.github.com>
2025-05-05 23:04:52 -0400

    fix: provided correct num of args for rx_gps_data and cleaned up rx_uart_data declaration

--- lib/uart/uart.h
+++ lib/uart/uart.h
@@ -3,11 +3,10 @@
 #include "driver/uart.h"
 
 #define RX_BUFFER_SIZE_256 256
-#define UART_2             UART_NUM_2
 #define UART_RX_PIN_16     16
 #define UART_TX_PIN_17     17
 #define BAUD_RATE_9600     9600
 
-void uart_init_8_bits(const int UART_NUM, const int UART_RX_PIN, const int UART_TX_PIN, const int RX_BUFFER_SIZE, const int BAUD_RATE);
-void rx_uart_data(const int UART_NUM, const int RX_BUFFER_SIZE, int* rx_bytes, uint8_t* data, int ms);
+void uart_init_8_bits(const int UART_NUM_2, const int UART_RX_PIN, const int UART_TX_PIN, const int RX_BUFFER_SIZE, const int BAUD_RATE);
+int rx_uart_data(const int UART_NUM_2, const int RX_BUFFER_SIZE, uint8_t* data, int ms);
 #endif
--- src/gps/main.c
+++ src/gps/main.c
@@ -5,9 +5,7 @@
 #include "../../lib/uart/uart.h"
 #include "../../lib/gps/gps.h"
 
-static const int RX_RATE_MS = 200;
-
 void app_main() {
-    uart_init_8_bits(UART_2, UART_RX_PIN_16, UART_TX_PIN_17, RX_BUFFER_SIZE_256, BAUD_RATE_9600);
-    xTaskCreate(rx_gps_data, "rx_uart_data_task", 1024*2, (void *) RX_RATE_MS, configMAX_PRIORITIES-1, NULL);
+    uart_init_8_bits(UART_NUM_2, UART_RX_PIN_16, UART_TX_PIN_17, RX_BUFFER_SIZE_256, BAUD_RATE_9600);
+    xTaskCreate(rx_gps_data, "rx_uart_data_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 }
