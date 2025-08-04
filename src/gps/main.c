28b40371926b40b6bb690be1b2f2bbb301f1692a
Andrew Sayanlar <72060694+asayanlar@users.noreply.github.com>
2025-08-04 17:26:51 -0400

    refactor: changed main.c task name to rx_uart_task

--- src/gps/main.c
+++ src/gps/main.c
@@ -7,5 +7,5 @@
 
 void app_main() {
     uart_init_8_bits(UART_NUM_2, UART_RX_PIN_16, UART_TX_PIN_17, RX_BUFFER_SIZE_256, BAUD_RATE_9600);
-    xTaskCreate(rx_gps_data, "rx_uart_data_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(rx_gps_data, "rx_uart_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 }
